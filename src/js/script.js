f0f5aff2cff7e8e9b2929363170817716e2282dd
Artyom <saaworking@gmail.com>
2022-06-04 17:55:57 +0300

    Catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,35 @@ $(document).ready(function () {
 			},]
 	});
 });
+
+
+const catalog = document.querySelector('.catalog'),
+	catalogItem = catalog.querySelector('.catalog__item'),
+	catalogTabs = catalog.querySelector('.catalog__tabs'),
+	catalogContents = catalog.querySelectorAll('.catalog__content');
+
+catalogContents.forEach(item => {
+	item.addEventListener('click', (e) => {
+		e.preventDefault();
+		const target = e.target,
+			parent = target.closest('.catalog__item-wrapper');
+		if (target && target.matches('.catalog__item-link')) {
+			parent.classList.toggle('catalog__item-wrapper_active');
+		}
+	})
+});
+
+catalogTabs.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target && target.matches('.catalog__tab > span')) {
+		const parent = target.closest('.catalog__tab');
+		catalogTabs.querySelectorAll('.catalog__tab').forEach(tab=> tab.classList.remove('catalog__tab_active'));
+		parent.classList.add('catalog__tab_active');
+
+		catalogContents.forEach(item => {
+			item.classList.remove('catalog__content_active');
+		});
+
+		catalogContents[parent.getAttribute('data-tab-number') - 1].classList.add('catalog__content_active');
+	}
+});
