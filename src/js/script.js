e0f82ed3d359a7b75c49d569170c03e433d75dc7
Artyom <saaworking@gmail.com>
2022-06-08 13:16:36 +0300

    buttons script

--- src/js/script.js
+++ src/js/script.js
@@ -38,7 +38,7 @@ catalogTabs.addEventListener('click', (e) => {
 	const target = e.target;
 	if (target && target.matches('.catalog__tab > span')) {
 		const parent = target.closest('.catalog__tab');
-		catalogTabs.querySelectorAll('.catalog__tab').forEach(tab=> tab.classList.remove('catalog__tab_active'));
+		catalogTabs.querySelectorAll('.catalog__tab').forEach(tab => tab.classList.remove('catalog__tab_active'));
 		parent.classList.add('catalog__tab_active');
 
 		catalogContents.forEach(item => {
@@ -48,3 +48,47 @@ catalogTabs.addEventListener('click', (e) => {
 		catalogContents[parent.getAttribute('data-tab-number') - 1].classList.add('catalog__content_active');
 	}
 });
+
+
+// Modals
+
+const consultationBtns = document.querySelectorAll('[data-modal=consultation]'),
+	overlay = document.querySelector('.overlay'),
+	modals = overlay.querySelectorAll('.modal'),
+	forms = document.querySelectorAll('.form');
+
+forms.forEach(item => {
+	item.addEventListener('submit', (e) => {
+		e.preventDefault();
+		item.reset();
+	});
+});
+
+function openModal(modalType) {
+	overlay.classList.add('overlay_active');
+	document.querySelector(modalType).classList.add('modal_active');
+}
+
+consultationBtns.forEach(item => {
+	item.addEventListener('click', () => {
+		openModal('#consultation');
+	});
+});
+
+overlay.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target && target.matches('.cross') || target.matches('.overlay')) {
+		overlay.classList.remove('overlay_active');
+		modals.forEach(item => {
+			item.classList.remove('modal_active');
+		})
+	}
+});
+
+catalog.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target && target.matches('.catalog__btn')) {
+		openModal('#order');
+		document.querySelector('#order .modal__subtitle').textContent = `${target.closest('.catalog__item').querySelector('.catalog__item-name').textContent}`;
+	}
+})
